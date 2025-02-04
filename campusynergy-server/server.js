34cd824134c19a88e3a5abfe2c2fb37f13c722f7
Dilpreet Kaur <dhanjaldilpreet1@gmail.com>
2025-02-04 18:21:38 -0500

    mongodb connection

--- campusynergy-server/server.js
+++ campusynergy-server/server.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-require('dotenv').config();
-
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+const cors = require("cors");
+require("dotenv").config();
+const uri = process.env.MONGO_URI;
 const app = express();
 
 // Connect to MongoDB
 connectDB();
+
+mongoose
+  .connect(uri)
+  .then(() =>
+    app.listen(3001, () => {
+      console.log("Server started on port 3001");
+    })
+  )
+  .catch((err) => console.log(err));
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
